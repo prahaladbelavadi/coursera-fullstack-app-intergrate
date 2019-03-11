e03c9a902d7a56a8820a25e1c68ab683f96d812b
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2019-03-11 13:53:33 +0530

    add auth service; need to implement auth guards

--- front-end-angular-hong-kong-university-coursera/conFusion/src/app/services/auth.service.ts
+++ front-end-angular-hong-kong-university-coursera/conFusion/src/app/services/auth.service.ts
@@ -1,9 +1,116 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+import { baseURL } from '../shared/baseurl';
+import { catchError, map } from 'rxjs/operators';
+
+
+interface AuthResponse {
+  status: string;
+  success: string;
+  token: string;
+}
+
+interface JWTResponse {
+  status: string;
+  success: string;
+  user: any
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  tokenKey =  'JWT';
+  isAuthenticated: Boolean = false;
+  username: Subject<string> = new Subject<string>();
+  authToken: string = undefined;
+
+  constructor( private http: HttpClient, private processHTTPMsgService: ProcessHTTPMsgService) { }
+
+  checkJWTtoken(){
+    this.http.get<JWTResponse>(baseURL + 'users/checkJWTtoken')
+    .subscribe(res =>{
+      console.log('JWT Valid: ', res);
+      this.sendUsername(res.user.username);
+    }, err =>{
+      console.log('JWT token Invalid', err);
+      this.destroyUserCredentials();
+    });
+  }
+
+  sendUsername(name: string){
+    this.username.next(name);
+  }
+
+  clearUsername(){
+    this.username.next(undefined);
+  }
+
+  loadUserCredentials(){
+    const credentials = JSON.parse(localStorage.getItem(this.tokenKey));
+
+    console.log('loadUserCredentials ', credentials);
+
+    if(credentials && credentials.username !== undefined){
+      this.useCredentials(credentials);
+      if (this.authToken) {
+        this.checkJWTtoken();
+      }
+    }
+  }
+
+  storeUserCredentials(credentials: any) {
+    console.log('storeUserCredentials ', credentials);
+    localStorage.setItem(this.tokenKey, JSON.stringify(credentials));
+    this.useCredentials(credentials);
+  }
+
+  useCredentials(credentials: any) {
+    this.isAuthenticated = true;
+    this.sendUsername(credentials.username);
+    this.authToken = credentials.token;
+  }
+
+  destroyUserCredentials() {
+    this.authToken = undefined;
+    this.clearUsername();
+    this.isAuthenticated = false;
+    localStorage.removeItem(this.tokenKey);
+  }
+
+
+  signUp() {
+
+  }
+
+  logIn(user: any): Observable<any> {
+    return this.http.post<AuthResponse>(baseURL + 'users/login',
+      { 'username': user.username, 'password': user.password })
+      .pipe(map(res => {
+        this.storeUserCredentials({ username: user.username, token: res.token });
+        return { 'success': true, 'username': user.username };
+      }),
+        catchError(error => this.processHTTPMsgService.handleError(error)));
+  }
+
+
+  logOut() {
+    this.destroyUserCredentials();
+  }
+
+  isLoggedIn(): Boolean {
+    return this.isAuthenticated;
+  }
+
+  getUsername(): Observable<string> {
+    return this.username.asObservable();
+  }
+
+  getToken(): string {
+    return this.authToken;
+  }
+
 }
